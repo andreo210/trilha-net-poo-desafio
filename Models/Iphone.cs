391736b0e7d65e8dcd4acfa75e3fdf8997b404c1
Andre <andreo210@hotmail.com>
2024-05-12 11:00:53 -0300

    desafio orientação  objeto

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, int memoria, string modelo, string imei) : base(numero, memoria, modelo, imei)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345678",memoria: 32, modelo: "m12",imei:"3232321");
+nokia.Ligar();
+nokia.InstalarAplicativo("roblox");
+Console.WriteLine("\n");
+
+
+
+Console.WriteLine("Smartphone IPhone");
+Smartphone iphone = new Iphone(numero: "12345678",memoria: 32, modelo: "m12",imei:"3232321");
+iphone.Ligar();
+iphone.InstalarAplicativo("roblox");
